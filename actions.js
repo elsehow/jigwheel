349c4b3c75993e68563b7e51680bd08bb91d05ac
elsehow <yes@cosmopol.is>
2016-03-14 10:36:46 -0700

    refactored

--- actions.js
+++
@@ -1,9 +0,0 @@
-function setup (dispatcher, store) {
-	dispatcher.on('button-click', (ev) => {
-		store.n = store.n+1
-    dispatcher.emit('update', store)
-	}	)
-	return
-}
-
-module.exports = setup
--- store.js
+++
@@ -1,9 +0,0 @@
-// function to setup initial app state
-function setup () { 
-  // put the schema for your app state here
-	return {
-		n: 0 
-	}
-}
-
-module.exports = setup
