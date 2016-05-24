7dd1fffe25fd7fcd2d6a63b7c8b80d7dba409d47
elsehow <yes@cosmopol.is>
2016-05-24 09:46:06 -0700

    new ideas

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 // deps
+var yo = require('yo-yo')
 var ud = require('ud')
-var el = document.querySelector('#app')
-function declare (fn, store) {
-  var ml = require('main-loop')
-  var vd = require('virtual-dom')
-  var l = ml(store, fn, vd)
-  el.appendChild(l.target)
-  return l
+var appEl = document.querySelector('#app')
+function declare (fn, state) {
+  let el = fn(state)
+  appEl.appendChild(el)
+  return el
 }
 
 // global app state is also defonced
@@ -17,13 +16,48 @@ var render = require('./view.js')(store)
 // a function to run on setup
 function setup () {
   // clear the element html just in case
-  el.innerHTML = ''
+  appEl.innerHTML = ''
   // set up main-loop
-	var loop = declare(render, store.getState())
+	var el = declare(render, store.getState())
   // on changes to store, update main-loop
-  store.subscribe(() => 
-    loop.update(store.getState()))
+  store.subscribe(state =>  { // TODO state comes in on subscribe ? flux style ?
+    yo.update(el, render(store.getState()))
+  })
 }
 
 // TODO onready
 setup()
+
+
+// TODO major thing that bothers me...
+// TODO state is coupled with the way its updated!!
+// TODO a defonced state object is like an immutable state object
+// TODO we cant mutate it directly! we mutate the ways in which it is updated
+// TODO to me, why not make events a stream? we can do a macro of some sort
+
+/*
+  TODO heres my idea
+
+
+  1: declaring the view
+
+    let render = ud.defn(renderF(emitF))
+
+  2: declaring the store
+
+    let transformer = ud.defn(storeTrasnformer)
+    streamFrom(emitF)
+      .map(transformer, initalState)
+      .onValue(state => {
+        yo.update(el, render(state))
+      })
+
+
+  3: now transform works just like flux SHOULD ;)
+
+
+
+  "keflux" heh
+  major advantage is that hte state trasnform mechanism is now hotswappable
+
+*/
--- src/view.js
+++ src/view.js
@@ -1,12 +1,13 @@
-var h = require('virtual-dom/h')
+var yo = require('yo-yo')
 
 module.exports = (store) => {
   // a render fn that returns hyperscript
-  return function render (state) {
-    return h('div', [
-      h('h1', `clicked ${state} times`),
-      h('button', { onclick: handleClick }, 'click')
-    ])
+  return (state) => {
+    return yo`<div>
+      <h1>clicked ${state} times</h1>
+      <button onclick=${handleClick}>click!!</button>
+    </div>
+    `
 
     function handleClick (ev) {
       store.dispatch({type: 'INCREMENT'})
